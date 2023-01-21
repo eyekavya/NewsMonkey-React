777b02fe0caaf8d48debcb2c04865cf55e427210
eyekavya <kavyasrivastava7830@gmail.com>
2023-01-21 14:07:48 +0530

    modified index.html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return <></>;
+export class App extends Component {
+  render() {
+    return <div>App</div>;
+  }
 }
 
 export default App;
