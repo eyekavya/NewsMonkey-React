36024990dec58e187d87d7e6f2c44b738761ef2b
eyekavya <kavyasrivastava7830@gmail.com>
2023-01-21 21:33:15 +0530

    added json news data and news components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import News from "./components/News";
 
 export class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <News />
       </div>
     );
   }
