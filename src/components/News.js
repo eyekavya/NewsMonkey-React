0e4fad37121ec685ad6e24a09f238f659bebe594
eyekavya <kavyasrivastava7830@gmail.com>
2023-01-26 18:32:55 +0530

    used fetch api for news items

--- src/components/News.js
+++ src/components/News.js
@@ -66,6 +66,16 @@ export class News extends Component {
       loading: false,
     };
   }
+
+  async componentDidMount() {
+    let data = await fetch(
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=e74225b94c0e4e2e9badc4c07532da02"
+    );
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles });
+  }
+
   render() {
     return (
       <div className="container my-3">
@@ -76,8 +86,8 @@ export class News extends Component {
             return (
               <div className="col-md-4" key={e.url}>
                 <NewsItem
-                  title={e.title.slice(0, 45)}
-                  description={e.description.slice(0, 88)}
+                  title={e.title ? e.title.slice(0, 45) : ""}
+                  description={e.description ? e.description.slice(0, 88) : ""}
                   imageUrl={e.urlToImage}
                   newsUrl={e.url}
                 />
