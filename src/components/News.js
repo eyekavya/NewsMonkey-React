3605b542f7a4c2993b1c3a633d5f1004715ba5c1
eyekavya <kavyasrivastava7830@gmail.com>
2023-01-28 15:15:14 +0530

    added loading spinner

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
   constructor() {
@@ -15,8 +16,11 @@ export class News extends Component {
     let data = await fetch(
       `https://newsapi.org/v2/top-headlines?country=in&apiKey=e74225b94c0e4e2e9badc4c07532da02&page=1&pageSize=${this.props.pageSize}`
     );
+    this.setState({
+      loading: true,
+    });
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles });
+    this.setState({ articles: parsedData.articles, loading: false });
   }
 
   handlePrevClick = async () => {
@@ -26,31 +30,39 @@ export class News extends Component {
         this.state.page - 1
       }&pageSize=${this.props.pageSize}`
     );
+    this.setState({
+      loading: true,
+    });
     let parsedData = await data.json();
     this.setState({
       page: this.state.page - 1,
       articles: parsedData.articles,
+      loading: false,
     });
   };
 
   handleNextClick = async () => {
     if (
-      this.state.page + 1 >
-      Math.ceil(this.state.totalResults / this.props.pageSize)
+      !(
+        this.state.page + 1 >
+        Math.ceil(this.state.totalResults / this.props.pageSize)
+      )
     ) {
-      return;
-    } else {
       let data = await fetch(
         // "https://newsapi.org/v2/top-headlines?country=in&apiKey=e74225b94c0e4e2e9badc4c07532da02"
         `https://newsapi.org/v2/top-headlines?country=in&apiKey=e74225b94c0e4e2e9badc4c07532da02&page=${
           this.state.page + 1
         }&pageSize=${this.props.pageSize}`
       );
+      this.setState({
+        loading: true,
+      });
       let parsedData = await data.json();
       this.setState({
         page: this.state.page + 1,
         articles: parsedData.articles,
         totalResults: parsedData.totalResults,
+        loading: false,
       });
     }
   };
@@ -59,20 +71,24 @@ export class News extends Component {
     return (
       <div className="container my-3">
         <h1 className="text-center">NewsMonkey - Top Headlines</h1>
+        {this.state.loading && <Spinner />}
         <div className="row">
-          {this.state.articles.map((e) => {
-            // a unique key is required in order to use map fn
-            return (
-              <div className="col-md-4" key={e.url}>
-                <NewsItem
-                  title={e.title ? e.title.slice(0, 45) : ""}
-                  description={e.description ? e.description.slice(0, 88) : ""}
-                  imageUrl={e.urlToImage}
-                  newsUrl={e.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((e) => {
+              // a unique key is required in order to use map fn
+              return (
+                <div className="col-md-4" key={e.url}>
+                  <NewsItem
+                    title={e.title ? e.title.slice(0, 45) : ""}
+                    description={
+                      e.description ? e.description.slice(0, 88) : ""
+                    }
+                    imageUrl={e.urlToImage}
+                    newsUrl={e.url}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
